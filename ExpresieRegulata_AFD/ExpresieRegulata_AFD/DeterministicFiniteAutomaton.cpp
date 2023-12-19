5c33d5699969d147edc14655af816ecbb3db2c3f
Roxana Sultan <roxana.sultan@student.unitbv.ro>
2023-12-19 10:18:22 +0200

    Implemented CheckWord function in DeterministicFiniteAutomaton class

--- ExpresieRegulata_AFD/ExpresieRegulata_AFD/DeterministicFiniteAutomaton.cpp
+++ ExpresieRegulata_AFD/ExpresieRegulata_AFD/DeterministicFiniteAutomaton.cpp
@@ -19,7 +19,34 @@ void DeterministicFiniteAutomaton::PrintAutomaton()
 
 bool DeterministicFiniteAutomaton::CheckWord(std::string word)
 {
-	return false;
+    for (auto letter : word)
+    {
+        if (std::ranges::find(m_Sigma, letter) == m_Sigma.end())
+        {
+            std::cout << "The letter is not part of Sigma!\n";
+            return false;
+        }
+    }
+    std::string currentQ = m_Initial;
+    while (word.size() > 0) 
+    {
+        auto it = m_Delta.find({ currentQ, word[0] });
+        if (it == m_Delta.end())
+        {
+            std::cout << "Jam!\n";
+            return false;
+        }
+        currentQ = it->second[0];
+        word.erase(word.begin(), word.begin() + 1);
+    }
+    for (auto index : m_Finals)
+    {
+        if (currentQ == index)
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
 bool DeterministicFiniteAutomaton::IsDeterministic()
