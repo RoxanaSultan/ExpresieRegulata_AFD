59d0b05e4d04c2f01a637435ff0e8d4189b4e65d
Livia-unitbv <livia.toma@student.unitbv.ro>
2023-12-19 10:20:44 +0200

    Implemented basic functions

--- ExpresieRegulata_AFD/ExpresieRegulata_AFD/NedeterministicFiniteAutomatonLamda.cpp
+++ ExpresieRegulata_AFD/ExpresieRegulata_AFD/NedeterministicFiniteAutomatonLamda.cpp
@@ -1,1 +1,44 @@
 #include "NedeterministicFiniteAutomatonLamda.h"
+
+NedeterministicFiniteAutomatonLamda::NedeterministicFiniteAutomatonLamda()
+{
+	m_Sigma.push_back('#');
+}
+
+NedeterministicFiniteAutomatonLamda::NedeterministicFiniteAutomatonLamda(const NedeterministicFiniteAutomatonLamda& afn) :
+	m_Q{ afn.m_Q },
+	m_Sigma{ afn.m_Sigma },
+	m_Delta{ afn.m_Delta },
+	m_Initial{ m_Initial },
+	m_Final{ afn.m_Final }
+{}
+
+NedeterministicFiniteAutomatonLamda& NedeterministicFiniteAutomatonLamda::operator=(const NedeterministicFiniteAutomatonLamda& afn)
+{
+	m_Q = afn.m_Q;
+	m_Sigma = afn.m_Sigma;
+	m_Delta = afn.m_Delta;
+	m_Initial = m_Initial;
+	m_Final = afn.m_Final;
+	return *this;
+}
+
+void NedeterministicFiniteAutomatonLamda::SetInitial(const std::string& initial)
+{
+	m_Initial = initial;
+}
+
+void NedeterministicFiniteAutomatonLamda::SetFinal(const std::string & final)
+{
+	m_Final = final;
+}
+
+std::string NedeterministicFiniteAutomatonLamda::GetInitial() const
+{
+	return m_Initial;
+}
+
+std::string NedeterministicFiniteAutomatonLamda::GetFinal() const
+{
+	return m_Final;
+}
--- ExpresieRegulata_AFD/ExpresieRegulata_AFD/NedeterministicFiniteAutomatonLamda.h
+++ ExpresieRegulata_AFD/ExpresieRegulata_AFD/NedeterministicFiniteAutomatonLamda.h
@@ -1,5 +1,26 @@
 #pragma once
+#include<iostream>
+#include<vector>
+#include<map>
 class NedeterministicFiniteAutomatonLamda
 {
+private:
+	std::vector<std::string> m_Q;
+	std::vector<char> m_Sigma;
+	std::map<std::pair<std::string, char>, std::vector<std::string>> m_Delta;
+	std::string m_Initial;
+	std::string m_Final;
+public:
+	NedeterministicFiniteAutomatonLamda();
+	NedeterministicFiniteAutomatonLamda(const NedeterministicFiniteAutomatonLamda& afn);
+	NedeterministicFiniteAutomatonLamda& operator=(const NedeterministicFiniteAutomatonLamda& afn);
+
+	//Setteri
+	void SetInitial(const std::string& initial);
+	void SetFinal(const std::string & final);
+
+	//Getteri
+	std::string GetInitial() const;
+	std::string GetFinal() const;
 };
 
